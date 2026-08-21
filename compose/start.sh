4f10159a9dea1745877eebd8ca3dc19d0e080215
gboege <44926950+gboege@users.noreply.github.com>
2026-08-21 14:43:04 +0200

    Minor edit

--- compose/start.sh
+++ compose/start.sh
@@ -24,7 +24,7 @@ FRONTEND_DEMO_USER_PASSWORD="$(grep '^FRONTEND_DEMO_USER_PASSWORD=' .env | cut -
 #
 if [ ! -f .env ]; then
     echo "No .env found."
-    echo "Creating .env from .env.template..."
+    echo "Creating .env from .env.example..."
     cp .env.example .env
 fi
 
